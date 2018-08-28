980f6dca4c0e9c80a97ecc24dc7e6ab189a724b1
zhanjiao <2508954163@qq.com>
2018-08-28 08:13:22 +0800

    项目第一次提交

---
+++ demo-test/src/main/java/cn/itcast/Demo.java
@@ -0,0 +1,14 @@
+package cn.itcast;
+
+/**
+ * Demo
+ *
+ * @author lee.siu.wah
+ * @version 1.0
+ * <p>File Created at 2018-08-27<p>
+ */
+public class Demo {
+    public static void main(String[] args){
+       System.out.println("=====1111======");
+    }
+}
